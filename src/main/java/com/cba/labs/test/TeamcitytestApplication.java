e2574c0af9d7c4e2c85d6a207d2cb384baed013c
marcus <marcus>
2015-11-16 18:06:06 +0700

    my first commit

---
+++ src/main/java/com/cba/labs/test/TeamcitytestApplication.java
@@ -0,0 +1,12 @@
+package com.cba.labs.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcitytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcitytestApplication.class, args);
+    }
+}
